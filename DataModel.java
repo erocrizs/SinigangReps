5aaf30f3261f1613831d58e8ff0d1a996458a414
erocrizs <aemielvin.loremia@gmail.com>
2017-01-26 12:17:18 +0800

    [feature] - implement DataModel class
    DataModel acts as the central body of the data being processed. Still
    incomplete.

---
+++ DataModel.java
@@ -0,0 +1,47 @@
+import java.io.*;
+
+public class DataModel {
+	
+	private Slide[] slides;
+	private MarkConfig markConfig;
+	private int currentSlide;
+	private int slideCount;
+	
+	public DataModel( File details, MarkConfig markConfig ) throws IOException {
+		this.markConfig = markConfig;
+		this.extractDetail( details );
+		this.currentSlide = 0;
+	}
+	
+	private extractDetail( File details ) throws IOException {
+		// TODO
+	}
+	
+	public int getCurrentSlideIndex() {
+		return currentSlide;
+	}
+
+	public void next() {
+		this.currentSlide++;
+	}
+	
+	public void previous() {
+		this.currentSlide--;
+	}
+	
+	public void reset() {
+		this.slides[ currentSlide ].reset();
+	}
+	
+	public void addMark( Vector mark ) {
+		this.slides[ currentSlide ].addMark( mark );
+	}
+	
+	public boolean[][] getScores() {
+		boolean[][] scores = new scores[ this.slideCount ][ 0 ];
+		for( int i=0; i < this.slideCount; i++ ) {
+			scores[i] = this.slides[i].getScore();
+		}
+		return scores;
+	}
+}
