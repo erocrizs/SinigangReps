1927e78c679e7e472a91f8399616849e6be8f308
erocrizs <aemielvin.loremia@gmail.com>
2017-01-26 11:43:38 +0800

    [implement] - implement Slide class
    Slide class represents a slide in the app. Stores the marks done to the
    slide, as well as the expected "correct answer" bugs. Can give the
    "score" of the marks.

---
+++ Slide.java
@@ -0,0 +1,35 @@
+public class Slide {
+	
+	private ArrayList<Vector> marks;
+	private ArrayList<BugBox> bugs;
+	private MarkConfig markConfig;
+	
+	public Slide( ArrayList<BugBox> bugs, MarkConfig markConfig ) {
+		this.bugs = bugs;
+		this.markConfig = markConfig;
+		this.marks = new ArrayList<Vector>();
+	}
+	
+	public void addMark( Vector newMark ) {
+		this.marks.add( newMark );
+	}
+	
+	public void reset() {
+		this.marks.clear();
+	}
+	
+	public boolean[] getScore() {
+		
+		boolean[] score = new boolean[ this.bugs.size() ];
+		for( int i=0; i<n; i++ ) {
+			for( Vector mark: this.marks ) {
+				if( markConfig.isEncircled( bugs.get( i ), mark ) ) {
+					score[i] = true;
+					break;
+				}
+			}
+		}
+		return score;
+	}
+	
+}
