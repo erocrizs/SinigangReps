99166a71f78fedc9cdd92d0e024d18a31ea62507
erocrizs <aemielvin.loremia@gmail.com>
2017-01-26 11:38:52 +0800

    [feature] - implement a Vector class
    just a vector class to store position and displacement

---
+++ Vector.java
@@ -0,0 +1,33 @@
+public class Vector {
+	private double x;
+	private double y;
+	
+	public Vector( double x, double y ) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double getX() {
+		return this.x;
+	}
+	
+	public double getY() {
+		return this.y;
+	}
+	
+	public Vector plus( Vector that ) {
+		double resultantX = this.getX() - that.getX();
+		double resultantY = this.getY() - that.getY();
+		return new Vector( resultantX, resultantY );
+	}
+	
+	public Vector minus( Vector that ) {
+		double resultantX = this.getX() - that.getX();
+		double resultantY = this.getY() - that.getY();
+		return new Vector( resultantX, resultantY );
+	}
+	
+	public double magnitude() {
+		return Math.sqrt( ( this.x * this.x ) + ( this.y * this.y ) );
+	}
+}
