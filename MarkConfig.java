b4441015f4fa30c10af17620c6e36572d68d1103
erocrizs <aemielvin.loremia@gmail.com>
2017-01-26 11:41:49 +0800

    [feature] - implement a MarkConfig class
    MarkConfig stores the configuration of the ellipse marker, i.e. its
    dimensions. Also has the utility to check collision between an ellipse
    and BugBox

---
+++ MarkConfig.java
@@ -0,0 +1,41 @@
+public class MarkConfig {
+	
+	private double a;
+	private double b;
+	
+	public MarkConfig( double horizontalAxis, double verticalAxis ) {
+		this.a = horizontalAxis / 2.0;
+		this.b = verticalAxis / 2.0;
+	}
+	
+	public boolean isEncircled( BugBox bug, Vector mark ) {
+		boolean encircled = true;
+		Vector[] corners = bug.getCorners();
+		
+		for( Vector corner: corners ) {
+			if( !this.collide( mark, corner ) ) {
+				encircled = false;
+				break;
+			}
+		}
+		
+		return encircled;
+	}
+	
+	private boolean collide( Vector ellipseCenter, Vector point ) {
+		Vector difference = point.minus( ellipseCenter );
+		
+		double firstTermNumerator = difference.getX();
+		double firstTermDenominator = a * a;
+		
+		double secondTermNumerator = difference.getY();
+		double secondTermDenominator = b * b;
+		
+		double firstTerm = firstTermNumerator / firstTermDenominator;
+		double secondTerm = secondTermNumerator / secondTermDenominator;
+		double eval = firstTerm + secondTerm;
+		
+		return eval <= 1;
+	}
+	
+}
